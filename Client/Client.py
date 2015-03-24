aa39296bf1a91df1d4ca5a5d8684934cc7be754e
Bang <mariusmbang@gmail.com>
2015-03-24 13:05:30 +0100

    MessageReceiver added to Client class, thread is runnable but currently doing nothing

--- Client/Client.py
+++ Client/Client.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import socket
 import json
+from MessageReceiver import *
 
 class Client:
     """
@@ -17,6 +18,9 @@ class Client:
         self.server_port = server_port
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.run()
+        self.messagereceiver = MessageReceiver(self, self.connection)
+        #self.messagereceiver.__init__()
+        #self.messagereceiver.start()
 
         # TODO: Finish init process with necessary code
 
